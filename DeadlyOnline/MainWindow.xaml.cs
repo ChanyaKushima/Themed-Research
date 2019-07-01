14923b415cb7b41c84069baedc00d2869e4874bf
ChanyaKushima <49159503+ChanyaKushima@users.noreply.github.com>
2019-07-01 14:45:03 +0900

    Add files via upload

--- DeadlyOnline/MainWindow.xaml.cs
+++ DeadlyOnline/MainWindow.xaml.cs
@@ -13,8 +13,10 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using System.Threading;
 using System.Net;
 using System.Net.Sockets;
+using System.Net.Security;
 using System.IO;
 using System.Runtime.CompilerServices;
 using System.Runtime.Serialization;
@@ -36,11 +38,18 @@ namespace DeadlyOnline
 	/// </summary>
 	public sealed partial class MainWindow : Window
 	{
-		readonly ClientData[] Clients = new ClientData[MaxClientsNo];
+        #region static field(s)
+        static readonly IPEndPoint IPEndPoint = new IPEndPoint(IPAddress.Any, Port);
+        #endregion
+
+
+        readonly ClientData[] Clients = new ClientData[MaxClientsNo];
 		readonly List<Task> ReadTasks = new List<Task>();
 		readonly Queue<object> ResultQueue = new Queue<object>();
 		TcpListener Listener;
 		Task ConnectAcceptTask;
+
+        long DataId = 0;
 
 		public MainWindow()
 		{
@@ -51,76 +60,79 @@ namespace DeadlyOnline
 		{
 			base.OnInitialized(e);
 
-			Listener = new TcpListener(IPAddress.Any, Port);
+			Listener = new TcpListener(IPEndPoint);
 			Listener.Start();
-			ConnectAcceptTask = Task.Run(ConnectionAccept);
+			ConnectAcceptTask = Task.Run(AcceptConnection);
 		}
 
-		private async Task ConnectionAccept()
+		private async Task AcceptConnection()
 		{
 			TcpClient client;
 
 			while (true)
 			{
-				int i = ArrayEmptyTer(Clients);
+				int i = GetArrayEmptyTerritory(Clients);
 
 				if (i >= Clients.Length) { continue; }
 
 				try { client = await Listener.AcceptTcpClientAsync(); }
 				catch (SocketException e)
 				{
-					Console.WriteLine($"{DateTime.Now.ToLongTimeString()}-- 受付中に例外が発生 {e.Message}");
+					Console.WriteLine($"{DateTime.Now}-- 受付中に例外が発生 {e.Message}");
 					continue;
 				}
 				
 				Clients[i] = new ClientData() { Client = client };
+                SendData(Clients[i], CommandFormat.Debug, new object[] { DateTime.Now, "接続完了" });
 
-				Task t = Task.Run(() => DataAccept(Clients[i], i));
+				Task t = Task.Run(() => AcceptData(Clients[i], i));
 				ReadTasks.Add(t);
 			}
 		}
 
-		private void DataAccept(ClientData clientData, int no)
-		{
-			BinaryFormatter formatter = new BinaryFormatter();
-			NetworkStream stream = clientData.Client.GetStream();
+        private void AcceptData(ClientData clientData, int no)
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            NetworkStream stream = clientData.Stream;
 
-			while (true)
-			{
-				object obj;
+            try
+            {
+                while (true)
+                {
+                    object obj = formatter.Deserialize(stream);
 
-				try
-				{
-					obj = formatter.Deserialize(stream);
+                    if (obj is ActionData ad)
+                    {
+                        Console.WriteLine($"{nameof(ActionData)}  {ad.Command}\tArgsCount:{ad.Arguments.Count()}");
 
-					if (obj is ActionData ad)
-					{
-						Console.WriteLine($"{nameof(ActionData)}  {ad.Command}\tArgsCount:{ad.Arguments.Count()}");
+                        var res = ActionCommandInvoke(ad);
+                        formatter.Serialize(stream, res);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{DateTime.Now} -- 不正な型のインスタンスが送られた");
+                        Disconnect(no);
+                        throw new NotImplementedException();
+                    }
+                }
+            }
+            catch (SerializationException ex)
+            {
+                Console.WriteLine($"{DateTime.Now} -- シリアル化、または逆シリアル化中に例外が発生 {ex.Message}");
+                Disconnect(no);
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine($"{DateTime.Now} -- ホストに強制的に切断されました {ex.Message}");
+                Disconnect(no);
+            }
+        }
 
-						var res = ActionCommandInvoke(ad);
-						formatter.Serialize(stream, res);
-					}
-					else if (obj is ResultData rd)
-					{
-						Console.WriteLine($"{nameof(ResultData)}  {rd.DataFormat}");
-						ResultCommandInvoke(rd);
-						ResultQueue.Enqueue(rd.Data);
-					}
-					else
-					{
-						Console.WriteLine($"{DateTime.Now.ToLongTimeString()} -- 不正な型のインスタンスが送られた");
-						Disconnect(no);
-						throw new NotImplementedException();
-					}
-				}
-				catch (SerializationException e)
-				{
-					Console.WriteLine($"{DateTime.Now.ToLongTimeString()} -- シリアル化、または逆シリアル化中に例外が発生 {e.Message}");
-					Disconnect(no);
-					throw;
-				}
-			}
-		}
+        private void SendData(ClientData client, CommandFormat cmd, IEnumerable<object> args = null, object data = null)
+        {
+            var ad = new ActionData(cmd, Interlocked.Increment(ref DataId), args, data);
+            ad.Send(client.Stream);
+        }
 
 		private void Disconnect(int clientNo)
 		{
