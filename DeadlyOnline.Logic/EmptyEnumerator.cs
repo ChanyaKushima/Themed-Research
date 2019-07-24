91f6476819909870db3599dc7aa9037c39fccdbe
九島茶にゃ <alicedomaincharlotte@gmail.com>
2019-07-25 00:16:09 +0900

    IEnumeratorから派生したクラス。 何も要素を持たないイテレータ

---
+++ DeadlyOnline.Logic/EmptyEnumerator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DeadlyOnline.Logic
+{
+    class EmptyEnumerator : IEnumerator
+    {
+        public static IEnumerator Instance => _instance ??= new EmptyEnumerator();
+
+        private static EmptyEnumerator _instance;
+
+        public object Current => throw new InvalidOperationException();
+
+        public bool MoveNext() => false;
+        public void Reset() { }
+    }
+}
