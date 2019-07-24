e3c3ff00a4a99cc32b3d9a113d0bb2f354b42ce7
九島茶にゃ <alicedomaincharlotte@gmail.com>
2019-07-25 00:17:09 +0900

    IEnumeratorから派生したクラス。 1つだけ要素を持つ。

---
+++ DeadlyOnline.Logic/SingleChildEnumerator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DeadlyOnline.Logic
+{
+    class SingleChildEnumerator : IEnumerator
+    {
+        private bool _hasNext;
+        private object _child;
+
+        public SingleChildEnumerator(object child)
+        {
+            _child = child;
+            _hasNext = _child != null;
+        }
+        public object Current => _hasNext ? _child : null;
+        public bool MoveNext()
+        {
+            bool hasNext = _hasNext;
+            _hasNext = false;
+            return hasNext;
+        }
+
+        public void Reset() => _hasNext = true;
+    }
+}
